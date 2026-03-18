c68cac71411d551d6dcdf63d448f108f6e957433
karolynne-freire <freirekarolynne@gmail.com>
2026-03-18 00:40:45 -0300

    feat: inclindo os documentos base

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Projeto ADS", page_icon="🚀")
+st.title("Meu Primeiro App com Streamlit")
+st.subheader("Desenvolvido por Karolynne - ADS IFPE")
+st.write("Este é um projeto acadêmico rodando direto da nuvem!")
