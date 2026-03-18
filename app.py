92bbdc9c46f75f8029bfcf7bb034db2ee2aa9b1b
karolynne-freire <freirekarolynne@gmail.com>
2026-03-18 00:56:07 -0300

    feat: inclindo os dados

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+from sklearn.ensemble import RandomForestRegressor
 
-st.set_page_config(page_title="Projeto ADS", page_icon="🚀")
-st.title("Meu Primeiro App com Streamlit")
+st.title("🏙️ Projeto Assistido: Boston")
 st.subheader("Desenvolvido por Karolynne - ADS IFPE")
-st.write("Este é um projeto acadêmico rodando direto da nuvem!")
+
+try:
+    data = pd.read_csv("data.csv")
+    st.write("Dados carregados com sucesso!")
+    
+    # Treinando o modelo (Slide 64)
+    x = data.drop("MEDV", axis=1)
+    y = data["MEDV"]
+    
+    rf_regressor = RandomForestRegressor()
+    rf_regressor.fit(x, y)
+    
+    st.write("### Tabela de Dados (Primeiras Linhas)")
+    st.dataframe(data.head())
+    
+    st.write("### Gráfico de Análise")
+    fig = px.scatter(data, x="RM", y="MEDV", title="Número de Quartos vs Preço")
+    st.plotly_chart(fig)
+
+except:
+    st.error("Ainda não achei o arquivo data.csv no seu GitHub! Suba ele lá para os dados aparecerem.")
